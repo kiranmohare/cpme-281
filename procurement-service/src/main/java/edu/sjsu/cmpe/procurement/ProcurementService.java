f1d504e2867c879a30ebb7c8eabfada5f6714a5b
kiranmohare <kiranmohare23@gmail.com>
2014-04-28 18:04:44 -0700

    new

--- procurement-service/src/main/java/edu/sjsu/cmpe/procurement/ProcurementService.java
+++ procurement-service/src/main/java/edu/sjsu/cmpe/procurement/ProcurementService.java
@@ -1,21 +1,14 @@
 package edu.sjsu.cmpe.procurement;
-import de.spinscale.dropwizard.jobs.JobsBundle;
-
-import edu.sjsu.cmpe.procurement.api.resources.RootResource;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import com.sun.jersey.api.client.Client;
 import com.yammer.dropwizard.Service;
-import com.yammer.dropwizard.client.JerseyClientBuilder;
 import com.yammer.dropwizard.config.Bootstrap;
 import com.yammer.dropwizard.config.Environment;
 
-import edu.sjsu.cmpe.procurement.api.resources.ProcurementServiceResource;
+import de.spinscale.dropwizard.jobs.JobsBundle;
 import edu.sjsu.cmpe.procurement.config.ProcurementServiceConfiguration;
-import com.yammer.dropwizard.client.JerseyClientBuilder;
-
 
 public class ProcurementService extends Service<ProcurementServiceConfiguration> {
 
@@ -28,27 +21,16 @@ public class ProcurementService extends Service<ProcurementServiceConfiguration>
     @Override
     public void initialize(Bootstrap<ProcurementServiceConfiguration> bootstrap) {
 	bootstrap.setName("procurement-service");
-	  bootstrap.addBundle(new JobsBundle("edu.sjsu.cmpe.procurement"));
-
+	bootstrap.addBundle(new JobsBundle("edu.sjsu.cmpe.procurement"));
     }
 
     @Override
     public void run(ProcurementServiceConfiguration configuration,
 	    Environment environment) throws Exception {
-    	
 	String queueName = configuration.getStompQueueName();
 	String topicName = configuration.getStompTopicName();
-	String apollouser=configuration.getApolloUser();
-	String apolloPassword=configuration.getApolloPassword();
-	String apollohost=configuration.getApolloHost();
-	String apolloPort=configuration.getApolloPort();
-	
-	log.debug("Queue name is {}. Topic name is {}. User is {}, password is {}. host is {}. port is {}", queueName,topicName,apollouser,apolloPassword,apollohost,apolloPort);
+	log.debug("Queue name is {}. Topic is {}", queueName, topicName);
 	// TODO: Apollo STOMP Broker URL and login
-//	final Client client=new JerseyClientBuilder().using(configuration.getJerseyClientConfiguration())
-//													.using(environment).build();
-	environment.addResource(RootResource.class);
-	environment.addResource(new ProcurementServiceResource());
 
     }
 }
